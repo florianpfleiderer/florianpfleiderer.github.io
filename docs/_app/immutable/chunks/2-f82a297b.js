ed59881f94cdd1c97c7d78799655603e4dadeb59
florianpfleiderer <florian@pfleiderer.at>
2024-10-05 19:04:28 +0100

    deleted docs

--- docs/_app/immutable/chunks/2-f82a297b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-678a32f8.js";export{t as component};
--- docs/_app/immutable/chunks/3-c7faaddd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/projects/_page.svelte-782e3dac.js";export{t as component};
